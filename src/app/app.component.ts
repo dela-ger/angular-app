35701e2e3628f1c3c84dba83b933b196f05c63fd
gdjamehkt <gdjameh@ktdevs.com>
2024-07-25 13:41:55 +0000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginformComponent } from './loginform/loginform.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, LoginformComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-app';
+}
